b84dbe00eb7c929723c9ea285dc1cd9a33e643c2
Fariha <farihaorna@gmail.com>
2024-12-18 19:22:41 -0500

    added header component with style

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import TrollImg from "../assets/images/troll-face.png"
+const Header = () => {
+  return(
+    <nav>
+      <img src={TrollImg}/>
+      <span>Meme Generator</span>
+    </nav>
+  )
+  }
+  
+export default Header;
